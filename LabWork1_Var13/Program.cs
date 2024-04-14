b4ff403d1fdb144c5552f72f13fc4bb967c0b4be
Nikita Melnik <iammelnikita.work@yandex.by>
2024-04-15 00:09:53 +0300

    Добавьте файлы проекта.

---
+++ LabWork1_Var13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace LabWork1_Var13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string:");
+            string input = Console.ReadLine();
+
+            while (input != "$stop")
+            {
+                ArrayList vowels = new ArrayList() { 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'y', 'Y', 'а', 'А', 'е', 'Е', 'ё', 'Ё', 'и', 'И', 'о', 'О', 'у', 'У', 'ы', 'Ы', 'э', 'Э', 'ю', 'Ю', 'я', 'Я' };
+
+                int i = 0;
+                while (i < input.Length)
+                {
+                    if (vowels.Contains(input[i]))
+                    {
+                        input = input.Insert(i, input[i].ToString());
+                        i += 2;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+
+                Console.WriteLine($"{input}\n");
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
